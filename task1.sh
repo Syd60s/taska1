736a7ae46abe1fc124f8724127113e9f61e831a7
Syd60s <ahmadbhq05@gmail.com>
2024-08-12 23:37:23 +0700

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -42,11 +42,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
+  defaultNetwork: "swisstronik",
   solidity: "0.8.19",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [\`0x\${process.env.PRIVATE_KEY}\`],
+      accounts: [`0x` + `${PRIVATE_KEY}`],
     },
   },
 };
